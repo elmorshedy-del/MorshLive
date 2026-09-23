2a4fb66f5c947fb731f9fd5d2ed82ae59afb9cbc
elmorshedy-del <darkmatter1339@gmail.com>
2026-09-22 22:08:03 -0400

    Revert ChatGPT Gulf detour: assets/js/i18n.js

--- assets/js/i18n.js
+++ assets/js/i18n.js
@@ -2,7 +2,7 @@
  * editorial, visual, content-architecture, and deterministic IPTV layers. */
 (function () {
   "use strict";
-  const stamp = "20260922gulfcup1";
+  const stamp = "20260913freeze1";
   const params = new URLSearchParams(location.search);
   const cleanPath = location.pathname.replace(/\/$/, "");
   const isWatchPage = cleanPath === "/watch.html" || cleanPath === "/watch";
